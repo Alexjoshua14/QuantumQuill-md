62c330a68e9f5a2b7c82cb88d2e45ae31fe55038
Alex <ajoshua14@icloud.com>
2023-12-30 13:22:35 -0800

    Add text selection events on focus and blur

--- src/components/documentControl/FileRename.tsx
+++ src/components/documentControl/FileRename.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { FC, HTMLAttributes, useState } from 'react'
+import { FC, HTMLAttributes, useRef, useState } from 'react'
 import { Input } from '../ui/input'
 import { cn } from '@/lib/utils'
 
@@ -19,17 +19,61 @@ interface FileRenameProps extends HTMLAttributes<HTMLDivElement> {
  */
 const FileRename: FC<FileRenameProps> = ({ docName, className, ...props }) => {
   const [documentName, setDocumentName] = useState<string>(docName)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const handleDocNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDocumentName(e.target.value)
   }
 
+  const handleDocNameBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    //setDocumentName(e.target.value)
+    try {
+      if (!inputRef.current) return
+
+      inputRef.current.selectionStart = 0
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const handleDocNameFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    try {
+      if (!inputRef.current) return
+
+      const fileExtensionLength = (inputRef.current.value.split('.').pop()?.length ?? -1) + 1
+
+      inputRef.current.selectionStart = 0
+      inputRef.current.selectionEnd = inputRef.current.value.length - fileExtensionLength
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div className={cn("h-full flex items-center gap-4 w-fit", className)} {...props}>
-      <Image src="/assets/icon-document.svg" alt="Document Icon" width={15} height={15} className="object-contain" />
+      <Image
+        src="/assets/icon-document.svg"
+        alt="Document Icon"
+        width={15} height={15}
+        className="object-contain"
+      />
       <div className="flex flex-col">
-        <p className="app-body-secondary text-gray-400">Document Name</p>
-        <Input id="doc-name" value={documentName} className="w-72 h-6 px-0 app-heading text-white font-light focus-visible:ring-0 border-0 hover:border-b-[1px] focus-visible:border-b-[1px] rounded-none" onChange={handleDocNameChange} />
+        <p className="app-body-secondary text-gray-400">
+          Document Name
+        </p>
+        <Input
+          id="doc-name"
+          value={documentName}
+          ref={inputRef}
+          className="min-w-36 md:min-w-52 lg:w-72 max-w-full h-6 px-0 
+                    app-heading text-white font-light rounded-none
+                    border-0 hover:border-b-[1px] 
+                    focus-visible:ring-0 focus-visible:border-b-[1px]"
+          onChange={handleDocNameChange}
+          onBlur={handleDocNameBlur}
+          onFocus={handleDocNameFocus}
+        />
       </div>
     </div>
   )
