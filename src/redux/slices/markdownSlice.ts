7fd9422a112e691e1d288c261e5ecef6a2747d32
Alex <ajoshua14@icloud.com>
2023-12-30 14:31:50 -0800

    Add shouldSave variable to context to allow components at any level to tell markdown editor component to initiate file save

--- src/redux/slices/markdownSlice.ts
+++ src/redux/slices/markdownSlice.ts
@@ -4,25 +4,55 @@ import type { RootState } from '../store'
 interface MarkdownState {
   filename: string
   content: string
+  shouldSave: boolean
 }
 
-const initialState: MarkdownState = {
-  filename: '',
-  content: '',
-}
+// const initialState: MarkdownState = {
+//   filename: '',
+//   content: '',
+// }
+
+const initialState: MarkdownState = (() => {
+  const data = require('../../data/data.json')
+  if (data) {
+    return {
+      filename: data[1].name,
+      content: data[1].content,
+      shouldSave: false
+    }
+  }
+
+  return {
+    filename: '',
+    content: '',
+    shouldSave: false
+  }
+})()
+
 
 export const markdownSlice = createSlice({
   name: 'markdown',
   initialState,
   reducers: {
-    setMarkdown: (state, action: PayloadAction<MarkdownState>) => {
-      state.filename = action.payload.filename
-      state.content = action.payload.content
+    setMarkdown: (state, action: PayloadAction<Partial<MarkdownState>>) => {
+      if (action.payload.filename)
+        state.filename = action.payload.filename
+      if (action.payload.content)
+        state.content = action.payload.content
     },
+    setFilename: (state, action: PayloadAction<string>) => {
+      state.filename = action.payload
+    },
+    setContent: (state, action: PayloadAction<string>) => {
+      state.content = action.payload
+    },
+    setShouldSave: (state, action: PayloadAction<boolean>) => {
+      state.shouldSave = action.payload
+    }
   },
 })
 
-export const { setMarkdown } = markdownSlice.actions
+export const { setMarkdown, setContent, setFilename, setShouldSave } = markdownSlice.actions
 
 export const selectMarkdown = (state: RootState) => state.markdown
 
