3f27cac3411fa1de3704ff9bd6671d85ebb61c16
Alex <ajoshua14@icloud.com>
2023-12-29 17:01:13 -0800

    Continue working on markdown and preview sections

--- src/components/markdown/MarkdownWrapper.tsx
+++ src/components/markdown/MarkdownWrapper.tsx
@@ -2,6 +2,8 @@
 
 import { FC } from 'react'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '../ui/resizable'
+import MarkdownSection from './MarkdownSection'
+import Preview from './Preview'
 
 interface MarkdownWrapperProps {
 
@@ -16,31 +18,13 @@ interface MarkdownWrapperProps {
 const MarkdownWrapper: FC<MarkdownWrapperProps> = ({ }) => {
 
   return (
-    <ResizablePanelGroup direction='horizontal' className="w-full min-h-[calc(100dvh-72px)]">
-      <ResizablePanel className="w-full min-h-full">
-        <section className="h-full flex flex-col">
-          <div className="w-full h-10 px-2 flex items-center bg-secondary">
-            <h2 className="app-heading-secondary">
-              Markdown
-            </h2>
-          </div>
-          <div className="h-full px-2">
-            some markdown goes here
-          </div>
-        </section>
+    <ResizablePanelGroup direction='horizontal' className="min-w-[300px] w-full min-h-[calc(100dvh-72px)] max-h-[calc(100dvh-72px)]">
+      <ResizablePanel className="min-h-full max-h-full">
+        <MarkdownSection />
       </ResizablePanel>
       <ResizableHandle withHandle className='bg-gray-300' />
-      <ResizablePanel className="w-full min-h-full">
-        <section className="h-full flex flex-col ">
-          <div className="w-full h-10 px-2 flex items-center  bg-secondary">
-            <h2 className="app-heading-secondary">
-              Preview
-            </h2>
-          </div>
-          <div className="h-full px-2">
-            some preview goes here
-          </div>
-        </section>
+      <ResizablePanel className="min-h-full max-h-full">
+        <Preview />
       </ResizablePanel>
     </ResizablePanelGroup>
   )
