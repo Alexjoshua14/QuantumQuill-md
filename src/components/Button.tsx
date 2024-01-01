53b6994f06343e33dfea5b8fbe76c926a3befe35
Alex <ajoshua14@icloud.com>
2024-01-01 11:46:18 -0800

    Center icon and text

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,7 +14,7 @@ interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
 
 const Button: FC<ButtonProps> = ({ text, icon, className, ...props }) => {
   return (
-    <button className={cn(`px-3 py-2 rounded flex gap-2 bg-orange-500 hover:bg-orange-300 text-white font-light`, className)} {...props}>
+    <button className={cn(`px-3 py-2 rounded flex items-center justify-center gap-2 bg-orange-500 hover:bg-orange-300 text-white font-light`, className)} {...props}>
       {icon && <Image src={icon.src} alt={icon.alt} width={icon.width ?? 16} height={icon.height ?? 16} className="object-contain" />}
       {text}
     </button>
