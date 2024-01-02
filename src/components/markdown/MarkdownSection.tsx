a151295f38739129b5eede7a498651c7e8564225
Alex <ajoshua14@icloud.com>
2024-01-01 18:28:32 -0800

    Add basic functionality to start allowing shortcut commands

--- src/components/markdown/MarkdownSection.tsx
+++ src/components/markdown/MarkdownSection.tsx
@@ -30,6 +30,9 @@ const MarkdownSection: FC<MarkdownProps> = ({ parentPanelRef, toggleShowPreview 
   const { content, shouldSave, saveFile } = useFile()
   const [localContent, setLocalContent] = useState<string>('')
   const { fullScreen } = useImperativePanelHandle(parentPanelRef ?? null)
+  const [commandQuery, setCommandQuery] = useState<string>('')
+  const [commandActive, setCommandActive] = useState<boolean>(false)
+  const [commandIndex, setCommandIndex] = useState<number>(0)
 
   useEffect(() => {
     setLocalContent(content)
@@ -47,6 +50,94 @@ const MarkdownSection: FC<MarkdownProps> = ({ parentPanelRef, toggleShowPreview 
     // console.log("Local content updated")
   }
 
+  const resetCommand = () => {
+    setCommandActive(false)
+    setCommandQuery('')
+    setCommandIndex(1)
+  }
+
+  /** 
+   * TODO: Move this logic to separate file and clean it up! 
+   * TODO: Handle user deleting multiple characters using shift/option/etc + arrow keys
+   * TODO: Handle user clicking on textarea to move cursor, this should just reset the command
+   * TODO: Handle user using undo/redo, this should just reset the command for now
+   * 
+   */
+  const checkForShortcuts = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (commandActive) {
+      if (e.key === 'Enter') {
+        e.preventDefault()
+        console.log("Command entered: ", commandQuery)
+        resetCommand()
+      } else if (e.key === 'Escape') {
+        e.preventDefault()
+        console.log("Command canceled")
+        resetCommand()
+      } else if (e.key === 'Backspace') {
+        if (commandQuery.length === 0) {
+          resetCommand()
+          console.log("Backed out of command")
+          return
+        }
+        if (commandQuery.length > commandIndex)
+          setCommandQuery(prev => commandQuery.slice(0, commandIndex - 1) + commandQuery.slice(commandIndex))
+        else
+          setCommandQuery(prev => commandQuery.slice(0, -1))
+
+        setCommandIndex(prev => prev - 1)
+
+      } else if (e.key === 'ArrowUp') {
+        e.preventDefault()
+        console.log("Up arrow pressed")
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault()
+        console.log("Down arrow pressed")
+      } else if (e.key === 'ArrowLeft') {
+        if (commandIndex > 0)
+          setCommandIndex(prev => prev - 1)
+        else {
+          console.log("Moving past beginning of command")
+          resetCommand()
+          return
+        }
+      } else if (e.key === 'ArrowRight') {
+        if (commandIndex < commandQuery.length)
+          setCommandIndex(prev => prev + 1)
+        else {
+          console.log("Moving past end of command")
+          resetCommand()
+          return
+        }
+
+      } else if (/^[a-zA-Z0-9]$/.test(e.key)) {
+
+        if (commandQuery.length > commandIndex)
+          setCommandQuery(prev => prev.slice(0, commandIndex) + e.key + prev.slice(commandIndex))
+        else
+          setCommandQuery(prev => prev + e.key)
+
+        setCommandIndex(prev => prev + 1)
+
+        /// TEMP CODE
+        if (commandQuery.length > commandIndex)
+          console.log("Command query: ", commandQuery.slice(0, commandIndex) + e.key + commandQuery.slice(commandIndex))
+        else
+          console.log("Command query: ", commandQuery + e.key)
+
+      }
+    } else if (e.key === '/') {
+      console.log("Looks like we have a hit!")
+      setCommandActive(true)
+    }
+  }
+
+  const handleClick = () => {
+    if (commandActive) {
+      console.log("Clicked textarea")
+      resetCommand()
+    }
+  }
+
   return (
     <section className="h-full overflow-y-auto flex flex-col">
       <TitleBar title="Markdown" onDoubleClick={fullScreen} toggleShowPreview={toggleShowPreview} />
@@ -56,6 +147,8 @@ const MarkdownSection: FC<MarkdownProps> = ({ parentPanelRef, toggleShowPreview 
             value={localContent}
             className="w-full h-full p-4 preview-markdown whitespace-pre-wrap resize-none focus:outline-none "
             onChange={updateMarkdown}
+            onKeyDown={checkForShortcuts}
+            onClick={handleClick}
           />
         </Suspense>
       </div>
