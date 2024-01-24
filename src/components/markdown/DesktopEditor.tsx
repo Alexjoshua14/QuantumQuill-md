e1681b88c8d3cda1cb725237661a1c3cd8ccba24
Alex <ajoshua14@icloud.com>
2024-01-23 16:28:20 -0800

    Merge min and max height

--- src/components/markdown/DesktopEditor.tsx
+++ src/components/markdown/DesktopEditor.tsx
@@ -14,7 +14,7 @@ const DesktopEditor: FC<DesktopEditorProps> = ({ }) => {
   const previewRef = useRef<ImperativePanelHandle>(null)
 
   return (
-    <ResizablePanelGroup direction='horizontal' className="min-w-[300px] w-full min-h-[calc(100dvh-72px)] max-h-[calc(100dvh-72px)]">
+    <ResizablePanelGroup direction='horizontal' className="min-w-[300px] w-full h-[calc(100dvh-72px)]">
       <ResizablePanel ref={mdRef} defaultSize={50} className="min-h-full max-h-full">
         <MarkdownSection parentPanelRef={mdRef} />
       </ResizablePanel>
